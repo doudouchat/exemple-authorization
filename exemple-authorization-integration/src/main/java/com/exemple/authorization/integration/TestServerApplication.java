f64b55521067f4e45252e74e0cebc79205e5bfd6
doudouchat <baraultb@hotmail.com>
2020-06-18 21:07:03 +0100

    remove code.revelc.net.zookeeper-maven-plugin

--- exemple-authorization-integration/src/main/java/com/exemple/authorization/integration/TestServerApplication.java
+++ exemple-authorization-integration/src/main/java/com/exemple/authorization/integration/TestServerApplication.java
@@ -1,7 +1,10 @@
 package com.exemple.authorization.integration;
 
+import org.apache.curator.test.TestingServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -20,6 +23,23 @@ public class TestServerApplication {
     @Configuration
     @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
     public static class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+    }
+
+    @Configuration
+    public static class ZookeeperConfiguration {
+
+        private final int port;
+
+        public ZookeeperConfiguration(@Value("${test.embedded.zookeeper.port}") int port) {
+            this.port = port;
+        }
+
+        @Bean(initMethod = "start", destroyMethod = "stop")
+        public TestingServer embeddedZookeeper() throws Exception {
+
+            return new TestingServer(port, false);
+        }
+
     }
 
 }
