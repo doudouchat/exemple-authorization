62091303a553ad3a929d329adcdb40559140a88e
doudouchat <baraultb@hotmail.com>
2020-11-25 14:55:05 +0000

    upgrade embedded-cassandra 4.0.0

--- exemple-authorization-resource/src/test/java/com/exemple/authorization/resource/core/ResourceTestConfiguration.java
+++ exemple-authorization-resource/src/test/java/com/exemple/authorization/resource/core/ResourceTestConfiguration.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
@@ -15,12 +16,10 @@ import org.springframework.core.io.ClassPathResource;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.exemple.authorization.resource.core.cassandra.ResourceCassandraConfiguration;
-import com.github.nosan.embedded.cassandra.EmbeddedCassandraFactory;
-import com.github.nosan.embedded.cassandra.api.Cassandra;
-import com.github.nosan.embedded.cassandra.api.connection.CassandraConnection;
-import com.github.nosan.embedded.cassandra.api.connection.CqlSessionCassandraConnectionFactory;
-import com.github.nosan.embedded.cassandra.api.cql.CqlDataSet;
-import com.github.nosan.embedded.cassandra.artifact.Artifact;
+import com.github.nosan.embedded.cassandra.Cassandra;
+import com.github.nosan.embedded.cassandra.CassandraBuilder;
+import com.github.nosan.embedded.cassandra.commons.logging.Slf4jLogger;
+import com.github.nosan.embedded.cassandra.cql.CqlScript;
 
 @Configuration
 @Import(ResourceConfiguration.class)
@@ -41,15 +40,17 @@ public class ResourceTestConfiguration extends ResourceCassandraConfiguration {
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Cassandra embeddedServer() {
 
-        EmbeddedCassandraFactory cassandraFactory = new EmbeddedCassandraFactory();
-        cassandraFactory.setArtifact(Artifact.ofVersion(version));
-        cassandraFactory.setPort(port);
-        cassandraFactory.getEnvironmentVariables().put("MAX_HEAP_SIZE", "64M");
-        cassandraFactory.getEnvironmentVariables().put("HEAP_NEWSIZE", "12m");
+        return new CassandraBuilder()
 
-        cassandraFactory.getConfigProperties().put("disk_failure_policy", "stop_paranoid");
+                .version(version)
 
-        return cassandraFactory.create();
+                .addEnvironmentVariable("MAX_HEAP_SIZE", "64M").addEnvironmentVariable("HEAP_NEWSIZE", "12m")
+
+                .addConfigProperty("native_transport_port", port).addConfigProperty("disk_failure_policy", "stop_paranoid")
+
+                .logger(new Slf4jLogger(LoggerFactory.getLogger("Cassandra")))
+
+                .build();
     }
 
     @Bean
@@ -74,11 +75,12 @@ public class ResourceTestConfiguration extends ResourceCassandraConfiguration {
     @PostConstruct
     public void initKeyspace() {
 
-        CqlSessionCassandraConnectionFactory cassandraConnectionFactory = new CqlSessionCassandraConnectionFactory();
+        CqlSession session = this.session();
 
-        try (CassandraConnection connection = cassandraConnectionFactory.create(embeddedServer())) {
-            CqlDataSet.ofClasspaths("cassandra/keyspace.cql", "cassandra/test.cql", "cassandra/exec.cql").forEachStatement(connection::execute);
-        }
+        CqlScript.ofClassPath("cassandra/keyspace.cql").forEachStatement(session::execute);
+        CqlScript.ofClassPath("cassandra/test.cql").forEachStatement(session::execute);
+        CqlScript.ofClassPath("cassandra/exec.cql").forEachStatement(session::execute);
+
     }
 
 }
