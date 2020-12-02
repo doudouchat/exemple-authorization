4fc680f25653ed275803dd0d15359803c9e2c400
doudouchat <38097871+doudouchat@users.noreply.github.com>
2020-12-02 12:54:40 +0100

    add endpoint .well-known/jwks.json (#11)

--- exemple-authorization-server/src/main/java/com/exemple/authorization/core/token/AuthorizationTokenConfiguration.java
+++ exemple-authorization-server/src/main/java/com/exemple/authorization/core/token/AuthorizationTokenConfiguration.java
@@ -1,11 +1,13 @@
 package com.exemple.authorization.core.token;
 
+import java.security.interfaces.RSAPublicKey;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ResourceLoader;
@@ -24,31 +26,30 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 
 import com.auth0.jwt.impl.PublicClaims;
 import com.hazelcast.core.HazelcastInstance;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.RSAKey;
 
 @Configuration
+@ComponentScan("com.exemple.authorization.core.token")
 public class AuthorizationTokenConfiguration {
 
     public static final String TOKEN_BLACK_LIST = "token.black_list";
 
-    private final String location;
-
     private final String alias;
 
-    private final String password;
-
-    private final ResourceLoader resourceLoader;
-
     private final HazelcastInstance hazelcastInstance;
+
+    private final KeyStoreKeyFactory keyStoreKeyFactory;
 
     public AuthorizationTokenConfiguration(ResourceLoader resourceLoader, HazelcastInstance hazelcastInstance,
             @Value("${authorization.certificat.location}") String location, @Value("${authorization.certificat.alias}") String alias,
             @Value("${authorization.certificat.password}") String password) {
 
-        this.location = location;
         this.alias = alias;
-        this.password = password;
-        this.resourceLoader = resourceLoader;
         this.hazelcastInstance = hazelcastInstance;
+        this.keyStoreKeyFactory = new KeyStoreKeyFactory(resourceLoader.getResource(location), password.toCharArray());
     }
 
     @Bean
@@ -59,8 +60,7 @@ public class AuthorizationTokenConfiguration {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(resourceLoader.getResource(location), password.toCharArray());
-        converter.setKeyPair(keyStoreKeyFactory.getKeyPair(alias));
+        converter.setKeyPair(this.keyStoreKeyFactory.getKeyPair(alias));
         converter.setJwtClaimsSetVerifier((Map<String, Object> claims) -> {
 
             Object jti = claims.get(PublicClaims.JWT_ID);
@@ -101,6 +101,13 @@ public class AuthorizationTokenConfiguration {
         defaultTokenServices.setTokenStore(tokenStore());
         defaultTokenServices.setSupportRefreshToken(true);
         return defaultTokenServices;
+    }
+
+    @Bean
+    public JWKSet jwkSet() {
+        RSAKey.Builder builder = new RSAKey.Builder((RSAPublicKey) this.keyStoreKeyFactory.getKeyPair(alias).getPublic()).keyUse(KeyUse.SIGNATURE)
+                .algorithm(JWSAlgorithm.RS256).keyID("exemple-key-id");
+        return new JWKSet(builder.build());
     }
 
 }
