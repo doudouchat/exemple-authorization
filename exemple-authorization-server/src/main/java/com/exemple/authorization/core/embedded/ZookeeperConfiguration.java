b12ca1bd1692c382ddddd75c0e92692ce5cb9cda
doudouchat <baraultb@hotmail.com>
2021-02-21 21:14:36 +0000

    embedded zookeeper

---
+++ exemple-authorization-server/src/main/java/com/exemple/authorization/core/embedded/ZookeeperConfiguration.java
@@ -0,0 +1,25 @@
+package com.exemple.authorization.core.embedded;
+
+import org.apache.curator.test.TestingServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(TestingServer.class)
+public class ZookeeperConfiguration {
+
+    private final int port;
+
+    public ZookeeperConfiguration(@Value("${zookeeper.embedded.port:-1}") int port) {
+        this.port = port;
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public TestingServer embeddedZookeeper() throws Exception {
+
+        return new TestingServer(port, false);
+    }
+
+}
