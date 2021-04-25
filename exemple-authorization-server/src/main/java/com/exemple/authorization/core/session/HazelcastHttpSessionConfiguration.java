1370cdd95d1d0e8372a9d889758d590c3c1e9030
doudouchat <38097871+doudouchat@users.noreply.github.com>
2021-04-25 23:14:59 +0200

    FIX missing Hazelcast4SessionUpdateEntryProcessor (#19)

--- exemple-authorization-server/src/main/java/com/exemple/authorization/core/session/HazelcastHttpSessionConfiguration.java
+++ exemple-authorization-server/src/main/java/com/exemple/authorization/core/session/HazelcastHttpSessionConfiguration.java
@@ -1,10 +1,18 @@
 package com.exemple.authorization.core.session;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.MapSession;
+import org.springframework.session.Session;
+import org.springframework.session.hazelcast.Hazelcast4SessionUpdateEntryProcessor;
 import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HttpSessionIdResolver;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
 
 @EnableHazelcastHttpSession
 @Configuration
@@ -13,6 +21,17 @@ public class HazelcastHttpSessionConfiguration {
     @Bean
     public HttpSessionIdResolver httpSessionIdResolver() {
         return HeaderHttpSessionIdResolver.xAuthToken();
+    }
+
+    @Bean
+    public HazelcastInstance client(@Value("${authorization.hazelcast.addresses}") String[] addresses) {
+        ClientConfig clientConfig = new ClientConfig();
+
+        clientConfig.getNetworkConfig().addAddress(addresses);
+        clientConfig.getUserCodeDeploymentConfig().setEnabled(true).addClass(Session.class).addClass(MapSession.class)
+                .addClass(Hazelcast4SessionUpdateEntryProcessor.class);
+
+        return HazelcastClient.newHazelcastClient(clientConfig);
     }
 
 }
