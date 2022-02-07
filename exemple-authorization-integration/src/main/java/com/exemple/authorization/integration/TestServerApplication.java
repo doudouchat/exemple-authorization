04683ca86d7d2aa99535fd5fceb6dae255d28982
doudouchat <baraultb@hotmail.com>
2022-02-07 11:30:04 +0000

    remove cloud security dependencies

--- exemple-authorization-integration/src/main/java/com/exemple/authorization/integration/TestServerApplication.java
+++ exemple-authorization-integration/src/main/java/com/exemple/authorization/integration/TestServerApplication.java
@@ -3,23 +3,13 @@ package com.exemple.authorization.integration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableResourceServer
 @ComponentScan("com.exemple.authorization.integration")
 public class TestServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestServerApplication.class, args);
-    }
-
-    @Configuration
-    @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-    public static class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
     }
 
 }
