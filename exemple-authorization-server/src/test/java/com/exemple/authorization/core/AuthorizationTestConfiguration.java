452480137892758ccaa19541ab370f2fc4e84d23
doudouchat <baraultb@hotmail.com>
2022-05-23 17:18:43 +0200

    remove jndi configuration

--- exemple-authorization-server/src/test/java/com/exemple/authorization/core/AuthorizationTestConfiguration.java
+++ exemple-authorization-server/src/test/java/com/exemple/authorization/core/AuthorizationTestConfiguration.java
@@ -1,30 +1,21 @@
 package com.exemple.authorization.core;
 
-import java.io.IOException;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
 import org.mockito.Mockito;
-import org.osjava.sj.SimpleJndi;
-import org.osjava.sj.loader.JndiLoader;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jndi.JndiObjectFactoryBean;
 
 import com.exemple.authorization.application.common.model.ApplicationDetail;
 import com.exemple.authorization.application.detail.ApplicationDetailService;
 import com.exemple.authorization.core.authentication.AuthenticationConfiguration;
 import com.exemple.authorization.core.client.AuthorizationClientTestConfiguration;
 import com.exemple.authorization.core.feature.FeatureTestConfiguration;
-import com.exemple.authorization.core.property.AuthorizationPropertyConfiguration;
 import com.exemple.authorization.core.resource.keyspace.AuthorizationResourceKeyspace;
 import com.exemple.authorization.core.session.HazelcastHttpSessionConfiguration;
 import com.exemple.authorization.core.swagger.SwaggerConfiguration;
@@ -38,7 +29,7 @@ import com.github.nosan.boot.autoconfigure.embedded.cassandra.EmbeddedCassandraA
         FeatureTestConfiguration.class })
 @ComponentScan(basePackageClasses = AuthorizationResourceKeyspace.class)
 @EnableAutoConfiguration(exclude = { CassandraAutoConfiguration.class, EmbeddedCassandraAutoConfiguration.class })
-public class AuthorizationTestConfiguration extends AuthorizationPropertyConfiguration {
+public class AuthorizationTestConfiguration {
 
     @Bean
     public LoginResource loginResource() {
@@ -61,24 +52,14 @@ public class AuthorizationTestConfiguration extends AuthorizationPropertyConfigu
     }
 
     @Bean
-    public InitialContext initialContext() throws NamingException, IOException {
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
 
-        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.osjava.sj.SimpleContextFactory");
-        System.setProperty(SimpleJndi.ENC, "java:comp");
-        System.setProperty(JndiLoader.COLON_REPLACE, "--");
-        System.setProperty(JndiLoader.DELIMITER, "/");
-        System.setProperty(SimpleJndi.SHARED, "true");
-        System.setProperty(SimpleJndi.ROOT, new ClassPathResource("java--comp").getURL().getFile());
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
 
-        return new InitialContext();
+        YamlPropertiesFactoryBean properties = new YamlPropertiesFactoryBean();
+        properties.setResources(new ClassPathResource("exemple-authorization-test.yml"));
 
-    }
-
-    @Bean
-    @DependsOn("initialContext")
-    @Override
-    public JndiObjectFactoryBean jndiObjectFactoryBean() {
-
-        return super.jndiObjectFactoryBean();
+        propertySourcesPlaceholderConfigurer.setProperties(properties.getObject());
+        return propertySourcesPlaceholderConfigurer;
     }
 }
