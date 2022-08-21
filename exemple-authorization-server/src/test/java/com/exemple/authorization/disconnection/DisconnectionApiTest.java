e2e76edbadda231729925a8796f3c366447bb703
doudouchat <baraultb@hotmail.com>
2022-08-21 22:09:03 +0100

    FIX DisconnectionApiTest Order

--- exemple-authorization-server/src/test/java/com/exemple/authorization/disconnection/DisconnectionApiTest.java
+++ exemple-authorization-server/src/test/java/com/exemple/authorization/disconnection/DisconnectionApiTest.java
@@ -13,9 +13,11 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -41,6 +43,7 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest(classes = { AuthorizationTestConfiguration.class }, webEnvironment = WebEnvironment.RANDOM_PORT)
 @Slf4j
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(OrderAnnotation.class)
 class DisconnectionApiTest {
 
     @Autowired
