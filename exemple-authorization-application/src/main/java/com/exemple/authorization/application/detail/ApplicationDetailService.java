9b01c6a9524b32c84f02b55526c336bb96fc5785
doudouchat <baraultb@hotmail.com>
2022-11-28 21:54:36 +0000

    remove useless interfaces

--- exemple-authorization-application/src/main/java/com/exemple/authorization/application/detail/ApplicationDetailService.java
+++ exemple-authorization-application/src/main/java/com/exemple/authorization/application/detail/ApplicationDetailService.java
@@ -1,14 +1,68 @@
 package com.exemple.authorization.application.detail;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.nodes.PersistentNode;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
 import com.exemple.authorization.application.common.model.ApplicationDetail;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public interface ApplicationDetailService {
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
-    void put(String application, JsonNode detail);
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ApplicationDetailService {
 
-    Optional<ApplicationDetail> get(String application);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @Qualifier("applicationDetailCuratorFramework")
+    private final CuratorFramework client;
+
+    @SneakyThrows
+    public Optional<ApplicationDetail> get(String application) {
+
+        try {
+
+            return Optional.of(MAPPER.readValue(client.getData().forPath("/" + application), ApplicationDetail.class));
+
+        } catch (KeeperException.NoNodeException e) {
+
+            LOG.warn("Application '" + application + "' not exists", e);
+
+            return Optional.empty();
+        }
+    }
+
+    public void put(String application, JsonNode detail) {
+
+        LOG.debug("Put detail {} for application {}", detail, application);
+
+        createDetail(application, detail);
+
+    }
+
+    private PersistentNode createDetail(String application, JsonNode detail) {
+
+        var node = new PersistentNode(client, CreateMode.PERSISTENT, false, "/" + application, detail.toString().getBytes(StandardCharsets.UTF_8));
+        node.start();
+
+        return node;
+
+    }
 
 }
--- exemple-authorization-resource/src/main/java/com/exemple/authorization/resource/login/LoginResource.java
+++ exemple-authorization-resource/src/main/java/com/exemple/authorization/resource/login/LoginResource.java
@@ -1,18 +1,61 @@
 package com.exemple.authorization.resource.login;
 
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+import org.springframework.stereotype.Service;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.exemple.authorization.resource.core.ResourceExecutionContext;
+import com.exemple.authorization.resource.login.dao.LoginDao;
 import com.exemple.authorization.resource.login.exception.UsernameAlreadyExistsException;
+import com.exemple.authorization.resource.login.mapper.LoginMapper;
 import com.exemple.authorization.resource.login.model.LoginEntity;
 
-public interface LoginResource {
+import lombok.RequiredArgsConstructor;
 
-    Optional<LoginEntity> get(String username);
+@Service
+@RequiredArgsConstructor
+public class LoginResource {
 
-    void update(LoginEntity login);
+    private final CqlSession session;
 
-    void save(LoginEntity login) throws UsernameAlreadyExistsException;
+    private final ConcurrentMap<String, LoginMapper> mappers = new ConcurrentHashMap<>();
 
-    void delete(String username);
+    public Optional<LoginEntity> get(String username) {
+
+        return Optional.ofNullable(dao().findByUsername(username));
+    }
+
+    public void update(LoginEntity source) {
+        dao().update(source);
+
+    }
+
+    public void save(LoginEntity source) throws UsernameAlreadyExistsException {
+        boolean notExists = dao().create(source);
+
+        if (!notExists) {
+
+            throw new UsernameAlreadyExistsException(source.getUsername());
+        }
+
+    }
+
+    public void delete(String username) {
+        dao().deleteByUsername(username);
+
+    }
+
+    private LoginDao dao() {
+
+        return mappers.computeIfAbsent(ResourceExecutionContext.get().keyspace(), this::build).loginDao();
+    }
+
+    private LoginMapper build(String keyspace) {
+
+        return LoginMapper.builder(session).withDefaultKeyspace(keyspace).build();
+    }
 
 }
